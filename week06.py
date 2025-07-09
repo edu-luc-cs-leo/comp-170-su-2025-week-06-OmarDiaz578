735a77ca1797708478c514baf22fed84f4d13ff4
OmarDiaz578 <odiaz5@luc.edu>
2025-07-09 02:25:43 +0000

    2nd commit

--- week06.py
+++ week06.py
@@ -2,12 +2,13 @@
 
 
 
-filepath = "/workspaces/comp-170-su-2025-week-06-OmarDiaz578/data/temperatures.txt"
+filepath = "/workspaces/comp-170-su-2025-week-06-OmarDiaz578/data/" #temperatures.txt"
+name = "temperatures.txt"
 
 #Load and Analyze Temperature Data
 def load_to_list(filepath: str) -> list[float]:
     final_list: list[float] = []
-    with open(filepath, "r") as temp_file:
+    with open(filepath+name, "r") as temp_file:
         for line in temp_file:
             value: float = float(line)
             final_list.append(value)
@@ -33,6 +34,29 @@ def descriptive_statistics(source_data: list[float]) -> None:
 
 source_data: list[float] = load_to_list(filepath)
 descriptive_statistics(source_data)
+
+filepath = "/workspaces/comp-170-su-2025-week-06-OmarDiaz578/data/"
+name = "markup.txt" 
+
+def apply_markup(filepath: str) -> None:
+    with open(filepath+name, "r") as mark_file:
+        lines: list[str] = mark_file.readlines()
+        for line in lines:
+            individual_words: list[str] = line.strip().split()
+            individual_line: str = ""
+            for word in individual_words:
+                if word.startswith("."):
+                    new_word: str = word[1:].upper()
+                elif word.startswith("_"):
+                    characters: list[str] = list(word[1:])
+                    new_word: str = " ".join(characters)
+                else:
+                    new_word: str = word
+                individual_line += new_word + " "
+            print(individual_line.strip())
+
+apply_markup(filepath)
+
 
 
 
