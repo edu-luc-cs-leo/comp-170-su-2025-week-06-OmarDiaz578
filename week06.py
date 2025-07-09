9608695efbd50c48de5a961b096d5e92d71e01c5
OmarDiaz578 <odiaz5@luc.edu>
2025-07-09 00:06:51 +0000

    1st

--- week06.py
+++ week06.py
@@ -1,5 +1,96 @@
 
 
+
+
+filepath = "/workspaces/comp-170-su-2025-week-06-OmarDiaz578/data/temperatures.txt"
+
+#Load and Analyze Temperature Data
+def load_to_list(filepath: str) -> list[float]:
+    final_list: list[float] = []
+    with open(filepath, "r") as temp_file:
+        for line in temp_file:
+            value: float = float(line)
+            final_list.append(value)
+    return final_list
+
+def descriptive_statistics(source_data: list[float]) -> None:
+    count: int = len(source_data)
+    total: float = 0.0
+    min_temp: float = source_data[0]
+    max_temp: float = source_data[0]
+
+    for temp in source_data:
+        total += temp
+        if temp < min_temp:
+            min_temp = temp
+        if temp > max_temp:
+            max_temp = temp
+    average: float = round(total / count, 2)
+    print(f"There are {count} values in the data source.")
+    print(f"The average value is {average}.")
+    print(f"The highest value is {max_temp} and the smallest value is {min_temp}.")
+
+
+source_data: list[float] = load_to_list(filepath)
+descriptive_statistics(source_data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #--------------------------------------------------------------------------------#
 # ⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎  WRITE YOUR CODE ABOVE THIS  LINE ⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎
 
